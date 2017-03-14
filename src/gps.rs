4667ac23582b2f3e4b2bbbcdaf82a8d6468389b1
Zeeshan Ali <zeenix@gmail.com>
2017-03-14 22:08:26 +0100

    Add gps module
    Currently it just tries to open the serial port device on the given path.

---
+++ src/gps.rs
@@ -0,0 +1,40 @@
+/* vim: set et ts=4 sw=4: */
+/* gps.rs
+ *
+ * Copyright (C) 2017 Pelagicore AB.
+ * Copyright (C) 2017 Zeeshan Ali.
+ *
+ * Geoclue is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * Geoclue is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Geoclue; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author: Zeeshan Ali <zeeshanak@gnome.org>
+ */
+
+use serial;
+
+pub struct GPS {
+    port: serial::SystemPort,
+}
+
+impl GPS {
+    pub fn new(path: &str) -> Result<Self, serial::Error> {
+        match serial::open(path) {
+            Ok(p) => {
+                Ok(GPS { port: p })
+            },
+
+            Err(e) => Err(e)
+        }
+    }
+}
